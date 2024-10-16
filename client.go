7fbe646085cc3b5cd5bb9c8e9b4978ee36427365
Seth Hoenig <shoenig@duck.com>
2024-10-15 22:23:47 -0500

    build: create Justfile and add basic targets

--- client.go
+++ client.go
@@ -1,3 +1,6 @@
+// Copyright (c) The Noxide Project Authors
+// SPDX-License-Identifier: BSD-3-Clause
+
 // Package memc provides a modern, efficient memcached client for Go.
 package memc
 
--- verbs.go
+++ verbs.go
@@ -1,3 +1,6 @@
+// Copyright (c) The Noxide Project Authors
+// SPDX-License-Identifier: BSD-3-Clause
+
 package memc
 
 import "errors"
