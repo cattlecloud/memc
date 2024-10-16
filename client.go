d58ca87371e979e7ed5577c7f2753c8fe377ecf5
Seth Hoenig <shoenig@duck.com>
2024-10-15 22:14:11 -0500

    client: encode and decode work

--- client.go
+++ client.go
@@ -3,6 +3,7 @@ package memc
 
 import (
 	"bytes"
+	"encoding/binary"
 	"encoding/gob"
 )
 
@@ -38,24 +39,108 @@ func encode(item any) ([]byte, error) {
 	case uint8:
 		b := byte(item.(uint8))
 		return []byte{b}, nil
-	case int16, uint16:
-	case int32, uint32:
-	case int64, uint64:
-	case int, uint:
+	case int16:
+		b := make([]byte, 2)
+		i := item.(int16)
+		binary.LittleEndian.PutUint16(b, uint16(i))
+		return b, nil
+	case uint16:
+		b := make([]byte, 2)
+		i := item.(uint16)
+		binary.LittleEndian.PutUint16(b, i)
+		return b, nil
+	case int32:
+		b := make([]byte, 4)
+		i := item.(int32)
+		binary.LittleEndian.PutUint32(b, uint32(i))
+		return b, nil
+	case uint32:
+		b := make([]byte, 4)
+		i := item.(uint32)
+		binary.LittleEndian.PutUint32(b, i)
+		return b, nil
+	case int64:
+		b := make([]byte, 8)
+		i := item.(int64)
+		binary.LittleEndian.PutUint64(b, uint64(i))
+		return b, nil
+	case uint64:
+		b := make([]byte, 8)
+		i := item.(uint64)
+		binary.LittleEndian.PutUint64(b, i)
+		return b, nil
+	case int:
+		b := make([]byte, 8)
+		i := item.(int)
+		binary.LittleEndian.PutUint64(b, uint64(i))
+		return b, nil
+	case uint:
+		b := make([]byte, 8)
+		i := item.(uint)
+		binary.LittleEndian.PutUint64(b, uint64(i))
+		return b, nil
+	default:
+		buf := new(bytes.Buffer)
+		enc := gob.NewEncoder(buf)
+		err := enc.Encode(item)
+		return buf.Bytes(), err
 	}
-	buf := new(bytes.Buffer)
-	enc := gob.NewEncoder(buf)
-	err := enc.Encode(item)
-	return buf.Bytes(), err
 }
 
-func decode[T any](b []byte, dest any) (T, error) {
-	switch dest.(type) {
+func decode[T any](b []byte) (T, error) {
+	var result T
+	switch any(result).(type) {
 	case []byte:
-		return dest.(T), nil
+		tmp := any(b).(T)
+		return tmp, nil
+	case string:
+		s := string(b)
+		tmp := any(s).(T)
+		return tmp, nil
+	case int8:
+		i := int8(b[0])
+		tmp := any(i).(T)
+		return tmp, nil
+	case uint8:
+		i := b[0]
+		tmp := any(i).(T)
+		return tmp, nil
+	case int16:
+		i := int16(binary.LittleEndian.Uint16(b))
+		tmp := any(i).(T)
+		return tmp, nil
+	case uint16:
+		i := binary.LittleEndian.Uint16(b)
+		tmp := any(i).(T)
+		return tmp, nil
+	case int32:
+		i := int32(binary.LittleEndian.Uint32(b))
+		tmp := any(i).(T)
+		return tmp, nil
+	case uint32:
+		i := binary.LittleEndian.Uint32(b)
+		tmp := any(i).(T)
+		return tmp, nil
+	case int64:
+		i := int64(binary.LittleEndian.Uint64(b))
+		tmp := any(i).(T)
+		return tmp, nil
+	case uint64:
+		i := binary.LittleEndian.Uint64(b)
+		tmp := any(i).(T)
+		return tmp, nil
+	case int:
+		i := int(binary.LittleEndian.Uint64(b))
+		tmp := any(i).(T)
+		return tmp, nil
+	case uint:
+		i := uint(binary.LittleEndian.Uint64(b))
+		tmp := any(i).(T)
+		return tmp, nil
+	default:
+		buf := bytes.NewBuffer(b)
+		dec := gob.NewDecoder(buf)
+		err := dec.Decode(&result)
+		return result, err
 	}
-	buf := bytes.NewBuffer(b)
-	dec := gob.NewDecoder(buf)
-	err := dec.Decode(dest)
-	return dest.(T), err
 }
--- client_test.go
+++ client_test.go
@@ -1,10 +1,16 @@
 package memc
 
 import (
+	"math"
 	"testing"
 
 	"github.com/shoenig/test/must"
 )
+
+type person struct {
+	Name string
+	Age  int
+}
 
 func Test_encode(t *testing.T) {
 	t.Run("[]byte", func(t *testing.T) {
@@ -33,9 +39,176 @@ func Test_encode(t *testing.T) {
 		must.SliceLen(t, 1, b)
 	})
 
-	// TODO all the types
+	t.Run("int16", func(t *testing.T) {
+		var i int16 = math.MaxInt16
+		b, err := encode(i)
+		must.NoError(t, err)
+		must.SliceLen(t, 2, b)
+	})
+
+	t.Run("uint16", func(t *testing.T) {
+		var i uint16 = math.MaxUint16
+		b, err := encode(i)
+		must.NoError(t, err)
+		must.SliceLen(t, 2, b)
+	})
+
+	t.Run("int32", func(t *testing.T) {
+		var i int32 = math.MaxInt32
+		b, err := encode(i)
+		must.NoError(t, err)
+		must.SliceLen(t, 4, b)
+	})
+
+	t.Run("uint32", func(t *testing.T) {
+		var i uint32 = math.MaxUint32
+		b, err := encode(i)
+		must.NoError(t, err)
+		must.SliceLen(t, 4, b)
+	})
+
+	t.Run("int64", func(t *testing.T) {
+		var i int64 = math.MaxInt64
+		b, err := encode(i)
+		must.NoError(t, err)
+		must.SliceLen(t, 8, b)
+	})
+
+	t.Run("uint64", func(t *testing.T) {
+		var i uint64 = math.MaxUint64
+		b, err := encode(i)
+		must.NoError(t, err)
+		must.SliceLen(t, 8, b)
+	})
+
+	t.Run("int", func(t *testing.T) {
+		var i int = math.MaxInt
+		b, err := encode(i)
+		must.NoError(t, err)
+		must.SliceLen(t, 8, b)
+	})
+
+	t.Run("uint", func(t *testing.T) {
+		var i uint = math.MaxUint
+		b, err := encode(i)
+		must.NoError(t, err)
+		must.SliceLen(t, 8, b)
+	})
+
+	t.Run("struct", func(t *testing.T) {
+		p := &person{
+			Name: "bob",
+			Age:  32,
+		}
+		b, err := encode(p)
+		must.NoError(t, err)
+		must.SliceLen(t, 48, b) // sure
+	})
 }
 
 func Test_decode(t *testing.T) {
-	// TODO
+	t.Run("[]byte", func(t *testing.T) {
+		result, err := decode[[]byte]([]byte{1, 2})
+		must.NoError(t, err)
+		must.Eq(t, []byte{1, 2}, result)
+	})
+
+	t.Run("string", func(t *testing.T) {
+		s := []byte("hello")
+		result, err := decode[string](s)
+		must.NoError(t, err)
+		must.Eq(t, "hello", result)
+	})
+
+	t.Run("int8", func(t *testing.T) {
+		result, err := decode[int8]([]byte{0xfe}) // little endian
+		must.NoError(t, err)
+		must.Eq(t, -2, result) // 2's compliment
+	})
+
+	t.Run("uint8", func(t *testing.T) {
+		result, err := decode[uint8]([]byte{0xff})
+		must.NoError(t, err)
+		must.Eq(t, math.MaxUint8, result)
+	})
+
+	t.Run("int16", func(t *testing.T) {
+		result, err := decode[int16]([]byte{0xfe, 0xff}) // little endian
+		must.NoError(t, err)
+		must.Eq(t, -2, result) // 2's compliment
+	})
+
+	t.Run("uint16", func(t *testing.T) {
+		result, err := decode[uint16]([]byte{0xff, 0xff})
+		must.NoError(t, err)
+		must.Eq(t, math.MaxUint16, result)
+	})
+
+	t.Run("int32", func(t *testing.T) {
+		result, err := decode[int32]([]byte{0xfe, 0xff, 0xff, 0xff}) // little endian
+		must.NoError(t, err)
+		must.Eq(t, -2, result) // 2's compliment
+	})
+
+	t.Run("uint32", func(t *testing.T) {
+		result, err := decode[uint32]([]byte{0xff, 0xff, 0xff, 0xff})
+		must.NoError(t, err)
+		must.Eq(t, math.MaxUint32, result)
+	})
+
+	t.Run("int64", func(t *testing.T) {
+		result, err := decode[int64]([]byte{0xfe, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff}) // little endian
+		must.NoError(t, err)
+		must.Eq(t, -2, result) // 2's compliment
+	})
+
+	t.Run("uint64", func(t *testing.T) {
+		result, err := decode[uint64]([]byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff})
+		must.NoError(t, err)
+		must.Eq(t, math.MaxUint64, result)
+	})
+
+	t.Run("int", func(t *testing.T) {
+		result, err := decode[int]([]byte{0xfe, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff}) // little endian
+		must.NoError(t, err)
+		must.Eq(t, -2, result) // 2's compliment
+	})
+
+	t.Run("uint", func(t *testing.T) {
+		result, err := decode[uint]([]byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff})
+		must.NoError(t, err)
+		must.Eq(t, math.MaxUint, result)
+	})
+
+	t.Run("struct pointer", func(t *testing.T) {
+		input, ierr := encode(&person{
+			Name: "bob",
+			Age:  32,
+		})
+		must.NoError(t, ierr)
+		must.NotNil(t, input)
+
+		result, err := decode[*person](input)
+		must.NoError(t, err)
+		must.Eq(t, &person{
+			Name: "bob",
+			Age:  32,
+		}, result)
+	})
+
+	t.Run("struct value", func(t *testing.T) {
+		input, ierr := encode(person{
+			Name: "alice",
+			Age:  30,
+		})
+		must.NoError(t, ierr)
+		must.NotNil(t, input)
+
+		result, err := decode[person](input)
+		must.NoError(t, err)
+		must.Eq(t, person{
+			Name: "alice",
+			Age:  30,
+		}, result)
+	})
 }
